950358e61cfda0d7b195a9973fd9a4bc00577ea3
Rahul Kashyap <rahul.kashyap@sportskeeda.com>
2021-08-01 18:59:49 +0530

    Adding comment for persistence level init

--- internal/persistence/init.go
+++ internal/persistence/init.go
@@ -2,6 +2,7 @@ package persistence
 
 import _ "github.com/kashyaprahul94/go-boilerplate/internal/persistence/mysql"
 
+// Placeholder init function that can be used to initialize any persistence service
 func init() {
 
 }
